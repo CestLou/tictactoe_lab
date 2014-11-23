4f1fb67d9d4478a7e9e2f84dab4f418aff3d6084
CestLou <alisonrebeccarobb@gmail.com>
2014-11-22 17:33:43 -0800

    More progress

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
 	var allDivs = document.querySelectorAll('.div');
-	document.querySelector('.div').addEventListener('click', function(){
-		for (i = 0; i<allDivs.length; i++){	
-			allDivs[i].addEventListener('click', function(){ 
-			// clickBehavior(this);
-			document.querySelector('#makeitclick').innerHTML = "X";
-			});
-		}
-// 		document.querySelector("h2").innerText = input + "\xb0F is equal to " + output + "\xB0C"
-	});
+	for (i = 0; i<allDivs.length; i++){	
+		allDivs[i].addEventListener('click', function(){ 
+		clickBehavior(this);
+		// document.querySelector('#makeitclick').innerHTML = "X";
+		});
+	}
+
 });
 
+var count = 0;
+
+var clickBehavior = function(theEl){
+	if (document.querySelector('#'+theEl.id).innerText === '') {
+		count+=1
+		document.querySelector('#'+theEl.id).innerText = (count%2 === 1) ? "X" : "O";
+	} else {
+		alert("No cheating!")
+	}
+}
+var reset = function() {
+
+}
